0c8ab7fb425c0b0aa7a85524a44d7ba9105a96ad
Ken Chan <KChan923@gmail.com>
2015-01-19 17:54:08 -0500

    created java frame to display video

--- src/CameraFrame.java
+++ src/CameraFrame.java
@@ -1,12 +1,52 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
 import org.opencv.highgui.VideoCapture;
 
-
-public class CameraFrame 
+public class CameraFrame extends JFrame implements ActionListener
 {
 	CameraFrame()
 	{
-		System.loadLibrary("overcv_java2410");
+		System.loadLibrary("opencv_java2410");
 		//	Capture a video. Parameter indicates the device ID of the camera on the PC.
 		VideoCapture list = new VideoCapture(0);
+		JMenu cameraMenu = new JMenu("Camera");
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.add(cameraMenu);
+		
+		int i = 1;
+		
+		//	Find all the cameras available on the local PC.
+		//	Create menu options to toggle amongst each camera.
+		while(list.isOpened())
+		{
+			JMenuItem cameraOption = new JMenuItem("Camera " + i);
+			cameraOption.addActionListener(this);
+			cameraMenu.add(cameraOption);
+			list.release();
+			list = new VideoCapture(i);
+			i++;
+		}
+		
+		setJMenuBar(menuBar);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(400, 400);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args)
+	{
+		CameraFrame cameraFrame = new CameraFrame();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
