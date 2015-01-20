a7e63c0979065529c98ee0b2a3a64941607e3308
Ken Chan <KChan923@gmail.com>
2015-01-19 20:42:57 -0500

    implemented screenshot feature

--- src/CameraPanel.java
+++ src/CameraPanel.java
@@ -3,9 +3,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 import org.opencv.core.Mat;
 import org.opencv.highgui.VideoCapture;
@@ -24,11 +29,27 @@ public class CameraPanel extends JPanel implements Runnable, ActionListener
 		add(screenshotButton);
 	}
 	
+	//	Capture a screenshot from the camera feed.
 	@Override
 	public void actionPerformed(ActionEvent e) 
 	{
 		// TODO Auto-generated method stub
-		
+		File output = new File("screenshot0.png");
+		int i = 0;
+		while(output.exists())
+		{
+			i++;
+			output = new File("screenshot" + i + ".png");
+		}
+		try 
+		{
+			ImageIO.write(image, "png", output);
+		} 
+		catch (IOException e1) 
+		{
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 	}
 
 	//	Read images captured by the camera, and draw them to the screen
@@ -48,6 +69,9 @@ public class CameraPanel extends JPanel implements Runnable, ActionListener
 				capture.read(webcam_image);
 				if(!webcam_image.empty())
 				{
+					//	Handle sizing of window.
+					JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
+					topFrame.setSize(webcam_image.width() + 20, webcam_image.height() + 40);
 					MatToBufferedImage(webcam_image);
 					repaint();
 				}
@@ -67,7 +91,7 @@ public class CameraPanel extends JPanel implements Runnable, ActionListener
 		g.drawImage(image, 10, 40, image.getWidth(), image.getHeight(), null);
 	}
 	
-	public void matToBufferedImage(Mat matBGR)
+	public void MatToBufferedImage(Mat matBGR)
 	{
 		int width = matBGR.width();
 		int height = matBGR.height();
